adf22aadfbbc7569ee15d3dd0aa546d1d9414c4a
cooper09 <cooper@sonyainc.net>
2015-10-19 11:35:48 -0400

    added generic google map

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.config(['$routeProvider', function($routeProvider) {
     }).
     when('/map', {
       templateUrl: 'partials/map.html',
-      controller: 'SearchController'
+      controller: 'MapController'
     }).
     otherwise({
       redirectTo: '/'
@@ -66,6 +66,94 @@ app.controller("DetailController", function($scope,  $http, $location ){
       };
 
 });//end detail controller
+
+app.controller("MapController", function($scope,  $http, $location ){
+  console.log("MapController");
+
+ //Data
+      var cities = [
+          {
+              city : 'Toronto',
+              desc : 'This is the best city in the world!',
+              lat : 43.7000,
+              long : -79.4000
+          },
+          {
+              city : 'New York',
+              desc : 'This city is aiiiiite!',
+              lat : 40.6700,
+              long : -73.9400
+          },
+          {
+              city : 'Chicago',
+              desc : 'This is the second best city in the world!',
+              lat : 41.8819,
+              long : -87.6278
+          },
+          {
+              city : 'Los Angeles',
+              desc : 'This city is live!',
+              lat : 34.0500,
+              long : -118.2500
+          },
+          {
+              city : 'Las Vegas',
+              desc : 'Sin City...\'nuff said!',
+              lat : 36.0800,
+              long : -115.1522
+          }
+      ];
+
+  //
+
+        var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(40.0000, -98.0000),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    $scope.markers = [];
+    
+    var infoWindow = new google.maps.InfoWindow();
+    
+    var createMarker = function (info){
+        
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(info.lat, info.long),
+            title: info.city
+        });
+        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+        
+        google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+            infoWindow.open($scope.map, marker);
+        });
+        
+        $scope.markers.push(marker);
+        
+    }  
+    
+    for (i = 0; i < cities.length; i++){
+        createMarker(cities[i]);
+    }
+
+    $scope.openInfoWindow = function(e, selectedMarker){
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    }
+
+
+  //
+   $scope.go = function ( path ) {
+        console.log("click for new view..." + path );
+        $location.path( path );
+      };
+
+});//end map controller
+
 
 app.controller("SearchController", function($scope,  $http, $location ){
   console.log("SearchController");
